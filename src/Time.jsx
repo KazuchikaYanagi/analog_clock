533f9944c0be36d25d7d5c3ce9b8a375e13f2a60
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-05-28 10:45:11 -0700

    create clock dir and time dir into component dir and move component files in component dir

---
+++ src/Time.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState } from "react";
+
+const Time = () => {
+  let time = new Data().toLocaleTimeString();
+
+  return <div>Time</div>;
+};
+
+export default Time;
